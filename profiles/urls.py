690ec6a2146299ef433afe5b3ae033d0deba9938
Wolf Howard <wolfahoward@outlook.com>
2017-08-11 17:38:51 -0500

    added first test.

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 app_name = 'profiles'
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
+	url(r'^$', views.IndexView.as_view(), name='index'),
 	url(r'^(?P<user_id>[0-9]+)/$', views.profile, name='profile'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,13 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
+from django.views import generic
 
 from .models import Profile
+	
 
-def index(request):
-	profile_list = Profile.objects.order_by('-pk')[:5]
-	context = {'profile_list': profile_list}
+class IndexView(generic.ListView):
+	template_name = 'profiles/index.html'
+	context_object_name = 'profile_list'
 
-	return render(request, 'profiles/index.html', context)
+	def get_queryset(self):
+		"""Return list of profiles ordered by primary key"""
+		return Profile.objects.order_by('-pk')
+
+# def index(request):
+# 	profile_list = Profile.objects.order_by('-pk')[:5]
+# 	context = {'profile_list': profile_list}
+
+# 	return render(request, 'profiles/index.html', context)
 
 def profile(request, user_id):
 
